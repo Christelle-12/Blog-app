84141f150c925c63b89f7e626a7919399b97b733
Chritelle-12 <mariechristellenirere@gmail.com>
2023-07-20 08:09:42 +0200

    Remove unwanted space

--- db/migrate/20230719081541_create_likes.rb
+++ db/migrate/20230719081541_create_likes.rb
@@ -1,4 +1,3 @@
-
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes, &:timestamps
--- db/migrate/20230719083904_rename_user_id_in_posts.rb
+++ db/migrate/20230719083904_rename_user_id_in_posts.rb
@@ -1,4 +1,3 @@
-
 class RenameUserIdInPosts < ActiveRecord::Migration[7.0]
   def change
     rename_column :posts, :user_id, :author_id
