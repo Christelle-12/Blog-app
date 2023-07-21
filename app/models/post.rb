c187f8435428a172583a6b5fc23109effcf97703
Chritelle-12 <mariechristellenirere@gmail.com>
2023-07-21 17:45:01 +0200

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,9 @@ class Post < ApplicationRecord
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  
 end
