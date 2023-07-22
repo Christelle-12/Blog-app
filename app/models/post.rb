59f0390960a06dbea571a925e9f5655fc0f5ffe5
Chritelle-12 <mariechristellenirere@gmail.com>
2023-07-22 11:53:16 +0200

    Add tests for models and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,22 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :likes, foreign_key: :post_id
-  has_many :comments, foreign_key: :post_id
+    belongs_to :author, class_name: 'User', foreign_key: :author_id
+    has_many :likes, foreign_key: :post_id
+    has_many :comments, foreign_key: :post_id
 
-  after_create :update_user_posts_counter
+    validates :title, presence: true, length: { maximum: 250 }
+    validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+    validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
-  def update_user_posts_counter
-    author.update(posts_counter: author.posts.count)
+    after_initialize do
+      self.comments_counter ||= 0
+      self.likes_counter ||= 0
+    end
+
+    def recent_comments
+      self.comments.order(created_at: :desc).limit(5)
+    end
+
+    def update_user_posts_counter
+      author.increment!(:posts_counter)
+    end
   end
-
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
-  
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
-  validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
-
-  def recent_posts
-    posts.order(created_at: :desc).limit(3)
+    has_many :posts, foreign_key: :author_id
+    has_many :likes, foreign_key: :author_id
+    has_many :comments, foreign_key: :author_id
+  
+    before_validation :initialize_posts_counter
+  
+    validates :name, presence: true
+    validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  
+    def recent_posts
+      posts.order(created_at: :desc).limit(3)
+    end
+  
+    def initialize_posts_counter
+      self.posts_counter ||= 0
+    end
+  
+    private
+  
   end
-end
+  
