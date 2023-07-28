134c7c26297f80a081670e5b5d1945137da6a1f3
Chritelle-12 <mariechristellenirere@gmail.com>
2023-07-28 10:03:17 +0200

    Updated Tests

--- spec/Requests/posts_controller_spec.rb
+++ spec/Requests/posts_controller_spec.rb
@@ -1,20 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /users/user_id/posts' do
+  describe 'GET /users/:user_id/posts' do
     it 'returns http success for the index action' do
-      get '/users/user_id/posts'
+      user = User.create(name: 'John Doe', photo: 'https://avatars.githubusercontent.com/u/123456789?v=4')
+      post1 = user.posts.create(title: 'My first post', text: 'This is my first post')
+      post2 = user.posts.create(title: 'My second post', text: 'This is my second post')
+
+      get "/users/#{user.id}/posts"
       expect(response.status).to eq(200)
       expect(response).to render_template(:index)
-      expect(response.body).to include('this is the main page of post')
+      expect(response.body).to include(post1.title)
+      expect(response.body).to include(post2.title)
     end
   end
-  describe 'GET /users/user_id/posts/:id' do
+
+  describe 'GET /users/:user_id/posts/:id' do
     it 'returns an http success' do
-      get '/users/user_id/posts/:id'
+      user = User.create(name: 'John Doe', photo: 'https://avatars.githubusercontent.com/u/123456789?v=4')
+      post = user.posts.create(title: 'My first post', text: 'This is my first post')
+
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response.status).to eq(200)
       expect(response).to render_template(:show)
-      expect(response.body).to include('this is the show page of post ')
+      expect(response.body).to include(post.title)
+      expect(response.body).to include(post.text)
     end
   end
 end
--- spec/Requests/users_controller_spec.rb
+++ spec/Requests/users_controller_spec.rb
@@ -3,18 +3,26 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
+      user1 = User.create(name: 'John Doe', photo: 'https://avatars.githubusercontent.com/u/123456789?v=4')
+      user2 = User.create(name: 'Jane Doe', photo: 'https://avatars.githubusercontent.com/u/987654321?v=4')
+
       get '/users'
       expect(response.status).to eq(200)
       expect(response).to render_template(:index)
-      expect(response.body).to include('this is the main page of user')
+      expect(response.body).to include(user1.name)
+      expect(response.body).to include(user2.name)
     end
   end
-  describe ' GET /show' do
+
+  describe 'GET /show' do
     it 'returns an http success' do
-      get '/users/:id'
+      user = User.create(name: 'John Doe', photo: 'https://avatars.githubusercontent.com/u/123456789?v=4')
+      get "/users/#{user.id}"
       expect(response.status).to eq(200)
       expect(response).to render_template(:show)
-      expect(response.body).to include('this is the show page of user')
+      expect(response.body).to include(user.name)
+      expect(response.body).to include(user.photo)
+      expect(response.body).to include(user.posts_counter.to_s)
     end
   end
 end
