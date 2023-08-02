058d73815f8dfbdebdde1e54c7bcb44d416d9879
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-02 12:14:22 +0200

    Add missing tests

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -24,8 +24,27 @@ RSpec.describe 'User show page', type: :feature do
       click_link 'Post'
       expect(page).to have_content(post.title)
     end
+    scenario 'I can see the user\'s bio.' do
+        user.update(bio: 'This is my bio.')
+  
+        visit user_path(user)
+        expect(page).to have_content(user.bio)
+      end
+  
+    scenario 'I can see a button that lets me view all of a user\'s posts.' do
+      visit user_path(user)
+      expect(page).to have_link('View All Posts', href: user_posts_path(user))
+    end
+
+    scenario 'When I click to see all posts, it redirects me to the user\'s post\'s index page.' do
+      create_list(:post, 5, user: user)
+
+      visit user_path(user)
+      click_link('View All Posts', href: user_posts_path(user))
+      expect(current_path).to eq(user_posts_path(user))
+      end
     scenario 'I can see the users first 3 posts'
-    visit user_path
-    expect(page).to have_content(user.posts.first(3))
+      visit user_path
+      expect(page).to have_content(user.posts.first(3))
   end
 end
