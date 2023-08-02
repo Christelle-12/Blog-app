de4c1aa42a51c96bf0c89e15c232879a9e0de4e4
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-02 14:00:26 +0200

    Add test for user_show last scenario

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -1,55 +1,54 @@
-require 'rails_helper'
-
 RSpec.describe 'User show page', type: :feature do
-  before do
-    @user = FactoryBot.create(:user) # Use FactoryBot to create a user
-    @post = FactoryBot.create(:post, author: @user)
-    @likes_count = 0
-    @comments_count = 0
-    @comments = [] # Initialize an empty array for comments for this test
+    let(:user) { create(:user) }
+    let(:post) { create(:post, user: user) }
+  
+    before do
+      scenario 'I can see the user profile picture.' do
+        visit user_path(user)
+        expect(page).to have_css("img[src='#{user.photo}']") if user.photo.present?
+      end
+  
+      scenario 'I can see the user name.' do
+        visit user_path(user)
+        expect(page).to have_content(user.name)
+      end
+  
+      scenario 'I can see the number of posts the user has written.' do
+        visit user_path(user)
+        expect(page).to have_content("Number of posts: #{user.posts.count}")
+      end
+  
+      scenario 'I can see the user bio.' do
+        visit user_path(user)
+        expect(page).to have_content(user.bio) if user.bio.present?
+      end
+  
+      scenario 'I can see the user\'s first 3 posts.' do
+        posts = create_list(:post, 5, user: user)
+        visit user_path(user)
+        expect(page).to have_content(posts.first.title)
+        expect(page).to have_content(posts.second.title)
+        expect(page).to have_content(posts.third.title)
+        expect(page).not_to have_content(posts.fourth.title)
+        expect(page).not_to have_content(posts.fifth.title)
+      end
+  
+      scenario 'I can see a button that lets me view all of a user\'s posts.' do
+        visit user_path(user)
+        expect(page).to have_link('View All Posts', href: user_posts_path(user))
+      end
+  
+      scenario 'When I click a user\'s post, it redirects me to that post\'s show page.' do
+        visit user_path(user)
+        click_link post.title
+        expect(page).to have_content(post.title)
+      end
+  
+      scenario 'When I click to see all posts, it redirects me to the user\'s post index page.' do
+        visit user_path(user)
+        click_link('View All Posts', href: user_posts_path(user))
+        expect(current_path).to eq(user_posts_path(user))
+      end
+    end
   end
-
-  scenario 'I can see the user profile picture.' do
-    @user.update(photo: 'http://example.com/user_profile_picture.jpg')
-    visit user_path(@user)
-    expect(page).to have_css("img[src='http://example.com/user_profile_picture.jpg']")
-  end
-
-  scenario 'I can see the user name.' do
-    visit user_path(@user)
-    expect(page).to have_content(@user.name)
-  end
-
-  scenario 'I can see the post details.' do
-    visit user_post_path(@user, @post)
-    expect(page).to have_content(@post.title)
-    expect(page).to have_content(@post.text)
-    expect(page).to have_content("Likes: #{@likes_count}")
-    expect(page).to have_content("Comments: #{@comments_count}")
-  end
-
-  # Update the scenario 'I can see the user\'s first 3 comments on the post.'
-  scenario 'I can see the user\'s first 3 comments on the post.' do
-    # Create 5 comments for the post with the associated user as the author
-    @comments = FactoryBot.create_list(:comment, 5, post: @post, author: @user)
-    visit user_post_path(@user, @post)
-    expect(page).to have_content(@comments[0].text)
-    expect(page).to have_content(@comments[1].text)
-    expect(page).to have_content(@comments[2].text)
-  end
-
-  scenario 'I can see a button to create a new comment on the post.' do
-    visit user_post_path(@user, @post)
-    expect(page).to have_link('Create Comment', href: new_user_post_comment_path(@user, @post))
-  end
-
-  scenario 'I can see a button to like the post.' do
-    visit user_post_path(@user, @post)
-    expect(page).to have_button('Like Post')
-  end
-  scenario 'When I click on a user, I am redirected to that users show page' do
-    visit user_path(@user)
-    click_on @user.name
-    expect(page).to have_content(@user.name)
-  end
-end
+  
