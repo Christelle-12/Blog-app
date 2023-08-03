450a8d20f138ee1c92ae1755b35bbb2d4ca54de2
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-03 20:38:46 +0100

    Remove extra lines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,12 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :photo, :bio, :email, :password])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo, :bio, :email, :password, :current_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name photo bio email password])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[name photo bio email password current_password])
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     # Replace 'user_path' with the path to the user page you want to redirect to.
     user_path(current_user)
   end
