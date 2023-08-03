d6b0ee8fc70d0f521ec10161bc423894395fbee7
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-03 18:52:44 +0200

    Add devise authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user = User.first
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Make current_user method available as a helper method for views
+  helper_method :current_user
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :photo, :bio, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo, :bio, :email, :password, :current_password])
+  end
+
+  def after_sign_in_path_for(resource)
+    # Replace 'user_path' with the path to the user page you want to redirect to.
+    user_path(current_user)
   end
 end
